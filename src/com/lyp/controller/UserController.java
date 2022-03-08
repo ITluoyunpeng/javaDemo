a3eaa182bc14bfb038bf68c655e917702fea0ef5
luoyunpeng <1825325715@qq.com>
2022-03-08 14:08:42 +0800

    我的天啊

---
+++ src/com/lyp/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.lyp.controller;
+
+public class UserController {
+    private String name;
+    private int sex;
+    public String eat(){
+        return null;
+    }
+
+    public String play(){
+        return null;
+    }
+}
